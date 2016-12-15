3e6baeca83ad81d833fb99b2a46e019a48145c98
Gonthar <maciej.gontar@gmail.com>
2016-12-15 17:15:54 +0100

    exceptions, create x2

--- virus_genealogy.h
+++ virus_genealogy.h
@@ -7,23 +7,26 @@
 #include <memory>
 #include <set>
 
-class VirusAlreadyCreated : std::runtime_error {
-public:
-	VirusAlreadyCreated()
-		: std::runtime_error("VirusAlreadyCreated") {}
-}
+class VirusNotFound : public std::exception {
+    const char* what() const noexcept
+    {
+        return "VirusNotFound";
+    }
+};
 
-class VirusNotFound : std::runtime_error {
-public:
-	VirusNotFound()
-		: std::runtime_error("VirusNotFound") {}
-}
+class VirusAlreadyCreated : public std::exception {
+    const char* what() const noexcept
+    {
+        return "VirusAlreadyCreated";
+    }
+};
 
-class TriedToRemoveStemVirus : std::runtime_error {
-public:
-	TriedToRemoveStemVirus()
-		: std::runtime_error("TriedToRemoveStemVirus") {}
-}
+class TriedToRemoveStemVirus : public std::exception {
+    const char* what() const noexcept
+    {
+        return "TriedToRemoveStemVirus";
+    }
+};
 
 template<class Virus>
 class VirusGenealogy {
@@ -71,27 +74,48 @@ public:
 
 	bool exists(const Virus::id_type& id) const noexcept
 	{
-		return virus_map.find(id) != virus_map.end();
+		return virus_map.find(id) != virus_map.end(); //TODO STRONG
 	}
 
 	Virus& operator[](const Virus::id_type& id) const
 	{
-		auto spt = find_map(id).lock();
+		auto spt = find_map(id).lock(); //TODO STRONG?
 		return *spt->virus_ptr;
 	}
 
 	void create(const Virus::id_type& id,
-				const Virus::id_type& parent_id) {
-		// TODO
+				const Virus::id_type& parent_id)
+    {
+        create(id, std::vector<const Virus::id_type>({parent_id}));
 	}
 
 	void create(const Virus::id_type& id,
-				const std::vector<Virus::id_type>& parent_ids) {
-		// TODO
+				const std::vector<Virus::id_type>& parent_ids)
+    {
+        if (exists(id))
+            throw VirusAlreadyCreated();
+        if (parent_ids.empty())
+            throw VirusNotFound();
+        for (Virus::id_type parent : parent_ids)
+        {
+            if (!exists(parent))
+                throw VirusNotFound();
+        }
+
+        Node v = Node(id);
+        std::weak_ptr<Node> v_ptr = std::make_shared<Node>(v);
+        for (Virus::id_type parent : parent_ids)
+        {
+            std::weak_ptr<Node> parent_ptr = find(parent);
+            v.add_parent(parent_ptr);
+            parent.add_child(v_ptr);
+        }
+        virus_map.insert(id, v_ptr);
 	}
 
 	void connect(const Virus::id_type& child_id,
-				 const Virus::id_type& parent_id) {
+				 const Virus::id_type& parent_id)
+    {
 		// TODO
 	}
 
@@ -124,24 +148,37 @@ private:
 	std::unordered_map<Virus::id_type, std::weak_ptr<Node>> virus_map;
 	std::shared_ptr<Node> stem_node;
 
-	struct Node {
+	class Node {
+    private:
+
 		Virus::id_type id;
 		std::unique_ptr<Virus> virus_ptr;
 		std::set<std::shared_ptr<Node>> children;
 		std::set<std::weak_ptr<Node>> parents;
 
-		Node(const Virus::id_type& vir_id)
-			: virus_ptr(new Virus(id))
-			, id(id) {}
-	};
+    public:
 
+		Node(const Virus::id_type& vir_id)
+			: virus_ptr(new Virus(id)), id(id) {
+        }
+
+        void add_parent(std::shared_ptr<Node> &parent) {
+            std::weak_ptr<Node> parent_ptr(parent); // TODO CONST
+            parents.insert(parent_ptr); // TODO STRONG
+        } // TODO STRONG OVERALL
+
+        void add_child(std::shared_ptr<Node> &child) {
+            children.insert(child); // TODO STRONG
+        } // TODO STRONG OVERALL
+    };
+    /*
 	std::weak_ptr<Node>& find_map(const Virus::id_type& id) const
 	{
 		try
 			return virus_map.at(id);
 		catch (const std::exception& e)
 			throw VirusNotFound();
-	}
+	}*/
 };
 
 #endif /* VIRUS_GENEALOGY_H */
